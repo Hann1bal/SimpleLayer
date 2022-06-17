00facf030ae6b1e51bd20f65bb9aa16134ddc8d1
milenium856 <milenium856@gmail.com>
2022-06-18 00:58:10 +0300

    Простой прокси сервер.

---
+++ Server/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Server;
+
+internal class Program
+{
+    private static ServerCore server; // сервер
+    private static Thread listenThread; // потока для прослушивания
+
+    private static void Main(string[] args)
+    {
+        try
+        {
+            server = new ServerCore();
+            listenThread = new Thread(server.Listen);
+            listenThread.Start(); //старт потока
+        }
+        catch (Exception ex)
+        {
+            server.Disconnect();
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
